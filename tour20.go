7c047f2933108c9d0fcdac4adebc394b306787d3
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-03 06:50:48 +0900

    finished tour20
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour20.go
@@ -0,0 +1,19 @@
+//http://go-tour-jp.appspot.com/#20
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	sum := 0
+	/*
+	for {
+	}
+*/
+
+	for i := 0 ; ; i++{
+		sum += 1
+		fmt.Println(sum)
+	}
+}
