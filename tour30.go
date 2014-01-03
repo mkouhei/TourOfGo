d18cbb91013a545b874549d6a8c25fea9ea92df2
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-04 07:42:24 +0900

    finished tour30
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour30.go
@@ -0,0 +1,18 @@
+//http://go-tour-jp.appspot.com/#30
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	p := []int{2, 3, 5, 7, 11, 13}
+	fmt.Println("p ==", p)
+
+	for i := 0; i < len(p); i++ {
+		fmt.Printf("p[%d] == %d\n",
+			i, p[i])
+	}
+	
+}
