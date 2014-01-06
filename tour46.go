375273891aeaa3a010825767d92228545f0b17e5
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-07 06:06:48 +0900

    finished tour46
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour46.go
@@ -0,0 +1,24 @@
+//http://go-tour-jp.appspot.com/#46
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+//	switch time.Saturday {
+	switch time.Tuesday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far awary.")
+	}
+}
