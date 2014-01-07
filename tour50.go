44e8cbf31e32f91b5ab950e07c2ad12577df93aa
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-08 06:09:02 +0900

    finished tour50
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour50.go
@@ -0,0 +1,22 @@
+//http://go-tour-jp.appspot.com/#49
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	fmt.Println(v.Abs())
+}
+
