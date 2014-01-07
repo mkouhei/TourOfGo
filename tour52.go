724b5abf3a8ff5625440cea66072711975904ff3
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-08 06:25:00 +0900

    finished tour52
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour52.go
@@ -0,0 +1,42 @@
+//http://go-tour-jp.appspot.com/#52
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+
+func (v Vertex) Scale2(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
+func (v Vertex) Abs2() float64 {
+	return math.Sqrt(v.X * v.X + v.Y * v.Y)
+}
+
+func main() {
+	v := &Vertex{3, 4}
+	v.Scale(5)
+	fmt.Println(v, v.Abs())
+
+	v2 := &Vertex{3, 4}
+	v2.Scale2(5)
+	fmt.Println(v2, v2.Abs2(), v.Abs2())
+}
+
