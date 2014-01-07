640c1c34161957977af830494f4765082e236b26
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-08 06:13:43 +0900

    finished tour51
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour51.go
@@ -0,0 +1,26 @@
+//http://go-tour-jp.appspot.com/#51
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+func main() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+
+	f2 := MyFloat(math.Sqrt2)
+	fmt.Println(f2.Abs())
+}
+
