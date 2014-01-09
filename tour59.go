509eebc4550e44e357f40bda93017121aff6d74d
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-09 09:52:11 +0900

    finished tour59
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour59.go
@@ -0,0 +1,14 @@
+//http://go-tour-jp.appspot.com/#59
+
+package main
+
+import (
+	"fmt"
+	"image"
+)
+
+func main() {
+	m := image.NewRGBA(image.Rect(0, 0, 100, 100))
+	fmt.Println(m.Bounds())
+	fmt.Println(m.At(0, 0).RGBA())
+}
