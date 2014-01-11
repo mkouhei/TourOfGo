043540b890ac7e43ff5bff249c283968f8b22139
Kouhei Maeda <mkouhei@palmtb.net>
2014-01-12 07:01:30 +0900

    finished tour70
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

---
+++ tour70.go
@@ -0,0 +1,49 @@
+//http://go-tour-jp.appspot.com/#70
+//https://gist.github.com/zyxar/2317744#file-exercise-tour-go-L220
+
+package main
+
+import (
+	"go-tour-72382f964b32/tree"
+	"fmt"
+)
+
+// Walk walks the tree t sending all values
+// from the tree to the channel ch.
+func Walk(t *tree.Tree, ch chan int) {
+	_walk(t, ch)
+	close(ch)
+}
+
+func _walk(t *tree.Tree, ch chan int) {
+	if t != nil {
+		_walk(t.Left, ch)
+		ch <- t.Value
+		_walk(t.Right, ch)
+	}
+}
+
+// Same determines whether the trees
+// t1 and t2 contain the same values.
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := range ch1 {
+		if i != <- ch2 {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	ch := make(chan int)
+	go Walk(tree.New(1), ch)
+	for v := range ch {
+		fmt.Print(v)
+	}
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
+}
